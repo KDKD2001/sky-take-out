d3736855c64e94e87a01dbd303a430871fdf218d
wkd <1424754342@qq.com>
2025-03-04 22:04:37 +0800

    分类管理模块功能

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -16,7 +16,7 @@ public interface CategoryMapper {
      * 插入数据
      * @param category
      */
-    @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
+    @Insert("insert into sky_take_out.category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Category category);
@@ -32,7 +32,7 @@ public interface CategoryMapper {
      * 根据id删除分类
      * @param id
      */
-    @Delete("delete from category where id = #{id}")
+    @Delete("delete from sky_take_out.category where id = #{id}")
     void deleteById(Long id);
 
     /**
