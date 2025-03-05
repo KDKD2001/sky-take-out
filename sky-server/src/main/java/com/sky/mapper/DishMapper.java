832ce5e27a38d502b1a0d9ba09dd153378ae6e0b
wkd <1424754342@qq.com>
2025-03-05 16:21:32 +0800

    新增菜品业务代码

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.Autofill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,4 +16,10 @@ public interface DishMapper {
     @Select("select count(id) from sky_take_out.dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    /**
+     * 新增菜品
+     * @param dish
+     */
+    @Autofill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
